c3ad14c8c576d3e60c20ca4016f11ca8e9c8adf0
AnthoMTL <140552957+AnthoMTL@users.noreply.github.com>
2025-03-26 07:15:59 -0400

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Purpose: To deploy the App to Cloud Run.
+
+# Google Cloud Project
+PROJECT=test-metal-358914
+
+# Google Cloud Region
+LOCATION=us-central1
+
+# Depolying app from source code
+#gcloud run deploy simple-app --source . --region=$LOCATION --project=$PROJECT --allow-unauthenticated
+gcloud run deploy simple-app --source . --region=us-central1 --project=test-metal-358914 --allow-unauthenticated
