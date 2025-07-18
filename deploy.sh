63ef5b27cb7b65dcaf36e19d9f83f5fd61572295
AnthoMTL <140552957+AnthoMTL@users.noreply.github.com>
2025-07-18 15:44:48 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,11 @@
 # Purpose: To deploy the App to Cloud Run.
 
 # Google Cloud Project
-PROJECT=test-metal-358914
+#PROJECT=YOUR PROJECT ID
 
 # Google Cloud Region
-LOCATION=us-central1
+#LOCATION=us-central1
 
 # Depolying app from source code
 #gcloud run deploy simple-app --source . --region=$LOCATION --project=$PROJECT --allow-unauthenticated
-gcloud run deploy simple-app --source . --region=us-central1 --project=test-metal-358914 --allow-unauthenticated 
-gcloud run deploy simple-app \
-  --source . \
-  --region=us-central1 \
-  --project=test-metal-358914 \
-  --cpu=4 \
-  --memory=2Gi \
-  --allow-unauthenticated
+gcloud run deploy simple-app --source . --region=us-central1 --project=YOUR-PROJECT-ID --allow-unauthenticated 
